a7b94faa779ef6f42fd4f17d4efb802cc17caf7a
Nicholas Escalona <nicholas.escalona@gmail.com>
2021-09-16 15:55:18 -0400

    changes from demo

--- email-app-ui/src/app/navbar/navbar.component.spec.ts
+++ email-app-ui/src/app/navbar/navbar.component.spec.ts
@@ -9,13 +9,16 @@ describe('NavbarComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [NavbarComponent],
+      providers: []
     }).compileComponents();
   });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(NavbarComponent);
     component = fixture.componentInstance;
+    //
     fixture.detectChanges();
+    //
   });
 
   it('should create', () => {
