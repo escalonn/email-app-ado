af6e6faa6731aff3004368cb534b4788ed51c890
Nicholas Escalona <nicholas.escalona@gmail.com>
2021-09-20 14:54:59 -0400

    fix unusual path in karma conf

--- email-app-ui/karma.conf.js
+++ email-app-ui/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true, // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require("path").join(__dirname, "./coverage/email-ui"),
+      dir: require("path").join(__dirname, "./coverage/email-app-ui"),
       subdir: ".",
       reporters: [{ type: "html" }, { type: "text-summary" }, { type: "lcov" }],
     },
